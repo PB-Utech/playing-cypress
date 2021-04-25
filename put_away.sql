2eb28d92e7e6e5cb2f05fece7a39ac559f031c6a
dariny <darin.y@utech.in.th>
2021-04-26 01:02:34 +0700

    add field for backup plan qr_product

--- put_away.sql
+++ put_away.sql
@@ -2,8 +2,9 @@
 select 
 upload_service.order_no, unload.product_no, 
 unload.revise_unit_desc, unload.revise_qty,
-concat('',split_log.lot_no) as lot_no,
-concat('',split_log.serial_no) as serial_no
+concat('',split_log.lot_no) as lotno,
+concat('',split_log.serial_no) as serialno,
+concat(upload_service.order_no,';',unload.product_no,';',split_log.lot_no,';',split_log.serial_no) as qr_product
 from act_trn_whop_unload as unload
 
 LEFT JOIN act_trn_whop_unload_split_log as split_log
@@ -23,7 +24,9 @@ order by product_no
 select 
 upload_service.order_no, unload.product_no, 
 unload.revise_unit_desc, unload.revise_qty,
-split_log.lot_no, split_log.serial_no
+split_log.lot_no, split_log.serial_no,
+concat(upload_service.order_no,';',unload.product_no,';',split_log.lot_no,';',split_log.serial_no) as qr_product
+
 from act_trn_whop_unload as unload
 
 LEFT JOIN act_trn_whop_unload_split_log as split_log
@@ -33,7 +36,7 @@ LEFT JOIN act_trn_order_input_upload_service as upload_service
 ON unload.order_id = upload_service.order_id
 where unload.order_Id
 in (
-	select order_id from act_trn_order_input_upload as iu where order_no = 'ORD202104200001'
+	select order_id from act_trn_order_input_upload as iu where order_no = 'ORD202104250004'
 ) and split_lot_flag = 'Y'
 order by product_no
 
@@ -42,7 +45,9 @@ order by product_no
 select 
 upload_service.order_no, unload.product_no, 
 unload.revise_unit_desc, unload.revise_qty,
-unload.lot_no
+unload.lot_no,
+concat(upload_service.order_no,';',unload.product_no,';',unload.lot_no,';') as qr_product
+
 from act_trn_whop_unload as unload
 
 LEFT JOIN act_trn_order_input_upload_service as upload_service
