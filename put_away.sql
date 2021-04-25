fb6305d5a4206bd3da9b25042ca27d39244ae7e3
dariny <darin.y@utech.in.th>
2021-04-26 00:48:34 +0700

    combine put_away data query into 1

--- put_away.sql
+++ put_away.sql
@@ -1,3 +1,25 @@
+/*IN ONE QUERY*/
+select 
+upload_service.order_no, unload.product_no, 
+unload.revise_unit_desc, unload.revise_qty,
+concat('',split_log.lot_no) as lot_no,
+concat('',split_log.serial_no) as serial_no
+from act_trn_whop_unload as unload
+
+LEFT JOIN act_trn_whop_unload_split_log as split_log
+ON unload.unload_id = split_log.unload_id
+
+LEFT JOIN act_trn_order_input_upload_service as upload_service
+ON unload.order_id = upload_service.order_id
+where unload.order_Id
+in (
+	select order_id from act_trn_order_input_upload as iu where order_no = 'ORD202104250004'
+)
+order by product_no
+
+/**************************************/
+/* SEPARATE Query Split and Not-Split */
+
 select 
 upload_service.order_no, unload.product_no, 
 unload.revise_unit_desc, unload.revise_qty,
